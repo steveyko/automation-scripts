b619bb115f7a055f79643b48c6267aea989424fc
Steve Ko <stevko@buffalo.edu>
2018-11-04 17:18:19 -0500

    scripts

---
+++ tophat.js
@@ -0,0 +1,239 @@
+const WAIT_TIMEOUT = 60000
+const GOTO_TIMEOUT = 60000
+const EXECUTION_TIMEOUT = 60000
+const PWD_FILE = 'pwd.in'
+const FILE_SUFFIX0 = '.0.xls'
+const FILE_SUFFIX1 = '.1.xls'
+
+const sections = {
+  '449785': 'A',
+  '999753': 'B',
+  '287174': 'C',
+  '096176': 'D',
+  '316904': 'E',
+  '517380': 'F',
+}
+
+function processFiles(xls0, xls1, section) {
+  const xlsReader = require('xlsx');
+  let workbookGrade = xlsReader.readFile(xls0)
+  let workbookAbsence = xlsReader.readFile(xls1)
+  let gradeSheet = workbookGrade.Sheets[workbookGrade.SheetNames[0]]
+  let absenceSheet = workbookAbsence.Sheets[workbookAbsence.SheetNames[0]]
+  let gradebook = {}
+
+  /*
+   * For grades,
+   *
+   * column C: email
+   * column D: first name
+   * column E: last name
+   * column G: grade
+   * column H: total
+   */
+  let row = 3
+  let total = gradeSheet['H3'].v
+  while (true) {
+    let email = (gradeSheet['C' + row] ? gradeSheet['C' + row].v : undefined)
+    let firstName = (gradeSheet['D' + row] ? gradeSheet['D' + row].v : undefined)
+    let lastName = (gradeSheet['E' + row] ? gradeSheet['E' + row].v : undefined)
+    let grade = (gradeSheet['G' + row] ? gradeSheet['G' + row].v : undefined)
+
+    if (email == undefined) break
+    if (grade == undefined) {
+      console.log('No grade for ' + email)
+      break
+    }
+
+    gradebook[email] = [section, email, firstName, lastName, grade, total]
+    row++
+  }
+
+  /*
+   * For absences,
+   *
+   * column E: email
+   * column K: absences
+   */
+  row = 2
+  while (true) {
+    let email = (absenceSheet['E' + row] ? absenceSheet['E' + row].v : undefined)
+    let absences = (absenceSheet['K' + row] ? absenceSheet['K' + row].v : undefined)
+
+    if (email == undefined) break
+    /*
+     * section, email, firstName, lastName, absences, grade, total
+     */
+    gradebook[email].splice(4, 0, absences)
+    row++
+  }
+
+  const fs = require('fs')
+  fs.unlink(xls0, (err) => { if (err) throw err })
+  fs.unlink(xls1, (err) => { if (err) throw err })
+
+  for (let email in gradebook) {
+    let data = gradebook[email]
+    console.log(data[0] + ',' + data[1] + ',' + data[2] + ',' + data[3] + ',' + data[4] + ',' + data[5] + ',' + data[6])
+  }
+}
+
+function setupNightmare(path) {
+  const Nightmare = require('nightmare')
+  require('nightmare-download-manager')(Nightmare)
+  let nightmare = Nightmare({
+    show: true,
+    paths: {
+      downloads: process.cwd(),
+    },
+    waitTimeout: WAIT_TIMEOUT,
+    gotoTimeout: GOTO_TIMEOUT,
+    executionTimeout: EXECUTION_TIMEOUT,
+  })
+
+  nightmare.on('console', (type, msg) => {
+    if (type == 'log') console.log(msg)
+    else console.error(msg)
+  })
+
+  nightmare.on('download', (state, downloadItem) => {
+    if (state == 'started') {
+      nightmare.emit('download', path, downloadItem)
+    }
+  })
+
+  return nightmare
+}
+
+function getUsernamePassword() {
+  const fs = require('fs')
+  let input = fs.readFileSync(process.cwd() + '/' + PWD_FILE, { encoding: 'utf8' })
+  input = input.split('\n')
+  let username = input[0].trim()
+  let password = input[1].trim()
+
+  return [username, password]
+}
+  
+async function getGrades(id, section)
+  const returnFilePath = process.cwd() + '/' + section + '.0.xls'
+  let nightmare = setupNightmare(returnFilePath)
+
+  let usernamePassword = getUsernamePassword()
+  let username = usernamePassword[0]
+  let password = usernamePassword[1]
+
+  await nightmare
+    .downloadManager()
+    .goto('https://myub.buffalo.edu')
+    .wait('input#login')
+    .insert('input#login', username)
+    .insert('input#password', password)
+    .click('input#login-button')
+    .wait(4000)
+    .goto('https://app.tophat.com/login')
+    .wait('input#login-school-select')
+    .wait(2000)
+    .insert('input#login-school-select', 'University at Buffalo SUNY')
+    .wait('.select-input__dropdown-option')
+    .wait(2000)
+    .click('.select-input__dropdown-option')
+    .wait('#flux-app > div > div.login-main__content > main > div.login-main__sso > div.ToolTipstyles__WithToolTip-s1rbkdy6-2.bKBYqH > button')
+    .wait(2000)
+    .click('#flux-app > div > div.login-main__content > main > div.login-main__sso > div.ToolTipstyles__WithToolTip-s1rbkdy6-2.bKBYqH > button')
+    .wait(3000)
+    .goto('https://app.tophat.com/e/' + id)
+    .wait('#flux-app > div > div > div.course_navigation_header__container > div.CourseHeaderstyles__CourseHeaderWrapper-s13b0dbv-0.fprTmH > nav > ul > li:nth-child(2) > a')
+    .wait(2000)
+    .click('#flux-app > div > div > div.course_navigation_header__container > div.CourseHeaderstyles__CourseHeaderWrapper-s13b0dbv-0.fprTmH > nav > ul > li:nth-child(2) > a')
+    .wait('#region-content > div > div > button.btn.btn-legacy.enter')
+    .wait(2000)
+    .click('#region-content > div > div > button.btn.btn-legacy.enter')
+    .wait('a.student > span:nth-child(2)')
+    .wait(2000)
+    .click('a.student > span:nth-child(2)')
+    .wait('#gradebook-export-dropdown > span.caret')
+    .wait(2000)
+    .click('#gradebook-export-dropdown > span.caret')
+    .wait('#gradebook-beta-app > div.header > div > ul > div.tab-button.dropdown.export-dropdown-button.open > ul > li:nth-child(2) > a > span')
+    .wait(2000)
+    .click('#gradebook-beta-app > div.header > div > ul > div.tab-button.dropdown.export-dropdown-button.open > ul > li:nth-child(2) > a > span')
+    .wait('#generate')
+    .wait(2000)
+    .click('#generate')
+    .wait('#export_gradebook_body > div > div.task_result > a')
+    .wait(2000)
+    .click('#export_gradebook_body > div > div.task_result > a')
+    .waitDownloadsComplete()
+    .end()
+    .then()
+    .catch((error) => {
+      console.error('Error:', error)
+    })
+
+  return returnFilePath
+}
+
+async function getAbsences(id, section)
+  const returnFilePath = process.cwd() + '/' + section + '.1.xls' 
+  let nightmare = setupNightmare(returnFilePath)
+
+  let usernamePassword = getUsernamePassword()
+  let username = usernamePassword[0]
+  let password = usernamePassword[1]
+
+  await nightmare
+    .downloadManager()
+    .goto('https://myub.buffalo.edu')
+    .wait('input#login')
+    .insert('input#login', username)
+    .insert('input#password', password)
+    .click('input#login-button')
+    .wait(4000)
+    .goto('https://app.tophat.com/login')
+    .wait('input#login-school-select')
+    .wait(2000)
+    .insert('input#login-school-select', 'University at Buffalo SUNY')
+    .wait('.select-input__dropdown-option')
+    .wait(2000)
+    .click('.select-input__dropdown-option')
+    .wait('#flux-app > div > div.login-main__content > main > div.login-main__sso > div.ToolTipstyles__WithToolTip-s1rbkdy6-2.bKBYqH > button')
+    .wait(2000)
+    .click('#flux-app > div > div.login-main__content > main > div.login-main__sso > div.ToolTipstyles__WithToolTip-s1rbkdy6-2.bKBYqH > button')
+    .wait(3000)
+    .goto('https://app.tophat.com/e/' + id)
+    .wait('#flux-app > div > div > div.course_navigation_header__container > div.CourseHeaderstyles__CourseHeaderWrapper-s13b0dbv-0.fprTmH > nav > ul > li:nth-child(2) > a')
+    .wait(2000)
+    .click('#flux-app > div > div > div.course_navigation_header__container > div.CourseHeaderstyles__CourseHeaderWrapper-s13b0dbv-0.fprTmH > nav > ul > li:nth-child(2) > a')
+    .wait('#region-content > div > div > button.btn.btn-legacy.enter')
+    .wait(2000)
+    .click('#region-content > div > div > button.btn.btn-legacy.enter')
+    .wait('a.student > span:nth-child(2)')
+    .wait(2000)
+    .click('a.student > span:nth-child(2)')
+    .wait('#gradebook-export-dropdown > span.caret')
+    .wait(2000)
+    .click('#gradebook-export-dropdown > span.caret')
+    .wait('.export-current-page > span:nth-child(2)')
+    .wait(2000)
+    .click('.export-current-page > span:nth-child(2)')
+    .waitDownloadsComplete()
+    .end()
+    .then()
+    .catch((error) => {
+      console.error('Error:', error)
+    })
+
+  return returnFilePath
+}
+
+async function main() {
+  for (let key in sections) {
+    let gradeFilePath = await getGrades(key, sections[key])
+    let absenceFilePath = await getAbsences(key, section[key])
+
+    await processFiles(gradeFilePath, absenceFilePath, section)
+  }
+}
+
+main()
---
+++ ublearns.js
@@ -0,0 +1,146 @@
+const EXPECTED_ACTIVITIES = 9
+const MAX_POINTS = 3
+const WAIT_TIMEOUT = 60000
+const GOTO_TIMEOUT = 60000
+const EXECUTION_TIMEOUT = 60000
+const PWD_FILE = 'pwd.in'
+
+const sections = {
+  '157324': 'A',
+  '157325': 'B',
+  '157326': 'C',
+  '157332': 'D',
+  '157335': 'E',
+  '157418': 'F',
+}
+
+const csvHeader = [
+  //'Last Name',
+  //'First Name',
+  //'Username',
+  //'Student ID',
+  //'Last Access',
+  //'Availability',
+  //'section [Total Pts: 0 Text]',
+  //'Group [Total Pts: 0 Text]',
+  //'Week 1 Recitation [Total Pts: 3 Score]',
+  'Week 2 Recitation - 8 Hours without the internet [Total Pts: 3 Score]',
+  'Social Media Unit - Recitation Session 1 [Total Pts: 3 Score]',
+  'Social Media Unit - Recitation Session 2 [Total Pts: 3 Score]',
+  'Signals Unit - Recitation Session 1 [Total Pts: 3 Score]',
+  'Signals Unit - Recitation Session 2 [Total Pts: 3 Score]',
+  'Embedded Systems Unit - Recitation Session 1 [Total Pts: 3 Score]',
+  'Embedded Systems Unit - Recitation Session 2 [Total Pts: 3 Score]',
+  //'Web Design Assignment [Total Pts: 3 Score]',
+  'Web Design Unit - Recitation Session 1 [Total Pts: 3 Score]',
+  'Web Design Unit - Recitation Session 2 [Total Pts: 3 Score]',
+  'Infrastructure Unit - Recitation Session 1 [Total Pts: 3 Score]',
+  'Infrastructure Unit - Recitation Session 2 [Total Pts: 3 Score]',
+  'Compression Unit - Recitation Session 1 [Total Pts: 3 Score]',
+  'Compression Unit - Recitation Session 2 [Total Pts: 3 Score]',
+]
+
+function cleanup(row) {
+  let newRow = {}
+  for (let key in row) {
+    csvHeader.forEach((curVal) => {
+      if (key.includes(curVal) && row[key] != '') {
+        newRow[curVal] = row[key]
+      }
+    })
+  }
+  return newRow
+}
+  
+async function runNightmare(id, section) {
+  const Nightmare = require('nightmare')
+  require('nightmare-download-manager')(Nightmare)
+  const nightmare = Nightmare({
+    show: true,
+    paths: {
+      downloads: process.cwd(),
+    },
+    waitTimeout: WAIT_TIMEOUT,
+    gotoTimeout: GOTO_TIMEOUT,
+    executionTimeout: EXECUTION_TIMEOUT,
+  })
+
+  nightmare.on('console', (type, msg) => {
+    if (type == 'log') console.log(msg)
+    else console.error(msg)
+  })
+
+  nightmare.on('download', (state, downloadItem) => {
+    let csv = process.cwd() + '/' + section + '.csv'
+    if(state == 'started') {
+      nightmare.emit('download', csv, downloadItem)
+    } else if (state == 'completed') {
+      const fs = require('fs')
+      const csvParser = require('csv-parser')
+      fs.createReadStream(csv)
+        .pipe(csvParser())
+        .on('data', (row) => {
+          for (let key in row) {
+            if (key.includes('Username')) let username = row[key]
+            if (key.includes('Last Name')) let lastname = row[key]
+            if (key.includes('First Name')) let firstname = row[key]
+            if (key.includes('ID')) let id = row[key] 
+          }
+          let newRow = cleanup(row)
+          let total = 0
+          for (let key in newRow) {
+            total += parseInt(newRow[key])
+          }
+
+          let max = EXPECTED_ACTIVITIES * MAX_POINTS
+          console.log(section + ',' + username + '@buffalo.edu,' + id + ',' + firstname + ',' + lastname + ',' + total + ',' + max)
+        })
+        .on('end', () => {
+          fs.unlink(csv, (err) => { if (err) throw err })
+        })
+    }
+  })
+
+  const fs = require('fs')
+  let input = fs.readFileSync(process.cwd() + '/' + PWD_FILE, { encoding: 'utf8' })
+  input = input.split('\n')
+  let username = input[0]
+  let password = input[1]
+
+  await nightmare
+    .downloadManager()
+    .goto('https://myub.buffalo.edu')
+    .wait('input#login')
+    .insert('input#login', username)
+    .insert('input#password', password)
+    .click('input#login-button')
+    .wait(4000)
+    .goto('https://ublearns.buffalo.edu')
+    .wait('#loginBox > a')
+    .wait(3000)
+    .click('#loginBox > a')
+    .wait(3000)
+    .goto('https://ublearns.buffalo.edu/webapps/gradebook/do/instructor/downloadGradebook?dispatch=viewDownloadOptions&course_id=_' + id + '_1')
+    .wait('#delimiterComma')
+    .wait(3000)
+    .check('#delimiterComma')
+    .wait('#bottom_submitButtonRow > input.submit.button-1')
+    .click('#bottom_submitButtonRow > input.submit.button-1')
+    .wait('#download_form > a')
+    .wait(3000)
+    .click('#download_form > a')
+    .waitDownloadsComplete()
+    .end()
+    .then()
+    .catch((error) => {
+      console.error('Error:' + error)
+    })
+}
+
+async function main() {
+  for (let key in sections) {
+    await runNightmare(key, sections[key])
+  }
+}
+
+main()
